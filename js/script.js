193254dfff00d32e7cbc5428718a14b961855ee5
arvio1378 <4rv10suhendar@gmail.com>
2023-07-11 13:11:28 +0700

    upload project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const side = document.querySelector('.navbar');
+const menu = document.querySelector('#bars');
+
+
+document.querySelector('#bars').onclick = () => {
+    side.classList.toggle('active');
+    menu.classList.toggle('fa-xmark');
+};
