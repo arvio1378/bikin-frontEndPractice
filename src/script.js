316704f00bf995c5bbd07b24f3dc49742dcdcfba
arvio1378 <4rv10suhendar@gmail.com>
2023-11-08 11:34:54 +0700

    update

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 const side = document.querySelector('.navbar');
 const menu = document.querySelector('#bars');
 
-
 document.querySelector('#bars').onclick = () => {
     side.classList.toggle('active');
     menu.classList.toggle('fa-xmark');
